0d26614eea35da7d23935374e74e362d884879e6
Shawn <wenchunyu@xindedata.com>
2018-11-14 19:44:30 +0800

    opt

--- src/core/trainer2.py
+++ src/core/trainer2.py
@@ -17,6 +17,14 @@ from tensorflow.keras import utils, models, layers, Sequential, Model, callbacks
 from src.constant import *
 
 
+def gen_one():
+    generator = ImageCaptcha(width=IMAGE_WIDTH, height=IMAGE_HEIGHT)
+    random_str = 'ajk8'
+    X = generator.generate_image(random_str)
+    X = np.expand_dims(X, 0)
+    return X
+
+
 def gen(batch_size=32):
     X = np.zeros((batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, 3), dtype=np.uint8)
     y = [np.zeros((batch_size, CHARACTERS_LENGTH), dtype=np.uint8) for i in range(CAPTCHA_LENGTH)]
@@ -44,6 +52,7 @@ def decode(y):
 # plt.close()
 
 
+# construct net
 input_tensor = layers.Input((60, 160, 3))
 x = input_tensor
 
@@ -64,32 +73,24 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adadelta',
               metrics=['accuracy'])
 
-my_callbacks = [
-    # Interrupt training if `val_loss` stops improving for over 2 epochs
-    callbacks.EarlyStopping(patience=2, monitor='val_loss'),
-    # Write TensorBoard logs to `./logs` directory
-    callbacks.TensorBoard(log_dir='./log'),
-    # callbacks.ModelCheckpoint(),
-]
-
 
 class MyCallback(callbacks.Callback):
 
     def __init__(self):
         super().__init__()
 
-    def set_params(self, params):
-        super().set_params(params)
-
-    def set_model(self, model):
-        super().set_model(model)
-
-    def on_epoch_begin(self, epoch, logs=None):
-        super().on_epoch_begin(epoch, logs)
+    # def set_params(self, params):
+    #     super().set_params(params)
+    #
+    # def set_model(self, model):
+    #     super().set_model(model)
+    #
+    # def on_epoch_begin(self, epoch, logs=None):
+    #     super().on_epoch_begin(epoch, logs)
 
     def on_epoch_end(self, epoch, logs=None):
         super().on_epoch_end(epoch, logs)
-        self.model.save_weights('./weights/my_model')
+        self.model.save_weights('./weight/my_model', False)
 
     def on_batch_begin(self, batch, logs=None):
         super().on_batch_begin(batch, logs)
@@ -104,38 +105,53 @@ class MyCallback(callbacks.Callback):
         super().on_train_end(logs)
 
 
-def evaluate(model, batch_num=10):
-    batch_acc = 0
-    generator = gen()
-    for i in range(batch_num):
-        [X_test, y_test, _, _], _ = next(generator)
-        y_pred = base_model.predict(X_test)
-        shape = y_pred[:, 2:, :].shape
-        ctc_decode = K.ctc_decode(y_pred[:, 2:, :],
-                                  input_length=np.ones(shape[0]) * shape[1])[0][0]
-        out = K.get_value(ctc_decode)[:, :4]
-        if out.shape[1] == 4:
-            batch_acc += ((y_test == out).sum(axis=1) == 4).mean()
-    return batch_acc / batch_num
+# def evaluate(model, batch_num=10):
+#     batch_acc = 0
+#     generator = gen()
+#     for i in range(batch_num):
+#         [X_test, y_test, _, _], _ = next(generator)
+#         y_pred = base_model.predict(X_test)
+#         shape = y_pred[:, 2:, :].shape
+#         ctc_decode = K.ctc_decode(y_pred[:, 2:, :],
+#                                   input_length=np.ones(shape[0]) * shape[1])[0][0]
+#         out = K.get_value(ctc_decode)[:, :4]
+#         if out.shape[1] == 4:
+#             batch_acc += ((y_test == out).sum(axis=1) == 4).mean()
+#     return batch_acc / batch_num
+#
+#
+# class Evaluate(callbacks.Callback):
+#
+#     def __init__(self):
+#         super().__init__()
+#         self.accs = []
+#
+#     def on_epoch_end(self, epoch, logs=None):
+#         acc = evaluate(base_model) * 100
+#         self.accs.append(acc)
+#         print('acc: %f%%' % acc)
+#
+#
+# evaluator = Evaluate()
+
+def train():
+    my_callbacks = [
+        # Interrupt training if `val_loss` stops improving for over 2 epochs
+        callbacks.EarlyStopping(patience=2, monitor='val_loss'),
+        # Write TensorBoard logs to `./logs` directory
+        callbacks.TensorBoard(log_dir='./log'),
+        # callbacks.ModelCheckpoint(),
+        MyCallback(),
+    ]
+
+    model.fit_generator(generator=gen(), steps_per_epoch=100, epochs=5, callbacks=my_callbacks,
+                        validation_data=gen(), validation_steps=20, workers=2)
 
 
-class Evaluate(callbacks.Callback):
-
-    def __init__(self):
-        super().__init__()
-        self.accs = []
-
-    def on_epoch_end(self, epoch, logs=None):
-        acc = evaluate(base_model) * 100
-        self.accs.append(acc)
-        print('acc: %f%%' % acc)
-
-
-evaluator = Evaluate()
-
-
-# model.fit_generator(generator=gen(), steps_per_epoch=100, epochs=5, callbacks=my_callbacks,
-#                     validation_data=gen(), validation_steps=20, workers=2)
+def predict():
+    model.load_weights('./weight/my_model')
+    y = model.predict(gen_one())
+    print(decode(y))
 
 
 def show_network():
@@ -144,9 +160,10 @@ def show_network():
 
 
 if __name__ == '__main__':
-    show_network()
+    # show_network()
     # X, y = next(gen(1))
     # plt.imshow(X[0])
     # plt.title(decode(y))
     # plt.show()
+    # predict()
     pass
